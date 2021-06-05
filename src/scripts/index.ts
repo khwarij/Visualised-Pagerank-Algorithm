c36705eff42de613fe5263a6ec9ccc4ce0c13e9b
Unknown807 <milvovangveric@gmail.com>
2021-06-05 22:36:58 +0100

    Using cytoscape, created a starting example

---
+++ src/scripts/index.ts
@@ -0,0 +1,15 @@
+const cytoscape = require("cytoscape");
+
+var cy = cytoscape({
+    container: document.getElementById('cy'),
+    elements: [
+      { data: { id: 'a' } },
+      { data: { id: 'b' } },
+      {
+        data: {
+          id: 'ab',
+          source: 'a',
+          target: 'b'
+        }
+      }]
+  });
